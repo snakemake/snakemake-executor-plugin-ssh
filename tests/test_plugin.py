e9ca489b0d611b78f1a97ca900b49420616c3c7e
Johannes Koester <johannes.koester@uni-due.de>
2025-03-26 15:20:05 +0100

    fixes

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,12 +1,13 @@
-from typing import Optional
+from typing import Mapping, Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
+from snakemake_interface_common.plugin_registry.plugin import TaggedSettings
 
 from snakemake_executor_plugin_ssh import ExecutorSettings
 
 
 # Check out the base classes found here for all possible options and methods:
-# https://github.com/snakemake/snakemake/blob/main/snakemake/common/tests/__init__.py
+# https://github.com/snakemake/snakemake/blob/main/src/snakemake/common/tests/__init__.py
 class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsBase):
     __test__ = True
 
@@ -19,3 +20,14 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsBase):
             hosts=["127.0.0.1"],
             identity_file="tests/sshserver/testkey",
         )
+    
+    def get_default_storage_provider(self) -> Optional[str]:
+        return "fs"
+
+    def get_default_storage_prefix(self) -> Optional[str]:
+        return ""
+    
+    def get_default_storage_provider_settings(
+        self,
+    ) -> Optional[Mapping[str, TaggedSettings]]:
+        return None
